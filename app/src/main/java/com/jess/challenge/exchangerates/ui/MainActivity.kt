5a005df74f8eadbeb06ae77ec758c6f1805ae3d0
jessica-cabarcas <jessica.cabarcas@globant.com>
2019-04-20 08:48:10 -0500

    Mobile-ui module created

---
+++ app/src/main/java/com/jess/challenge/exchangerates/ui/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.jess.challenge.exchangerates.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
